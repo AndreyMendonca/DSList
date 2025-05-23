3edfbea07f6dc8d6a23957d1e258f3e036102895
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-05-23 15:54:21 -0300

    Implementação do modelo de dominio da aplicação

---
+++ src/main/java/com/andrey/dslist/entities/Belonging.java
@@ -0,0 +1,55 @@
+package com.andrey.dslist.entities;
+
+import com.andrey.dslist.entities.pk.BelongingPK;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_beloning")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging () {}
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setGameList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id) && Objects.equals(position, belonging.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, position);
+    }
+}
